f407912977cf4db02cb5f5610c5e7fad522c4f4d
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-11-13 20:34:29 -0300

    crew page script

--- js/crew.js
+++ js/crew.js
@@ -6,3 +6,31 @@ window.addEventListener('click', e => {
         hideBtns.classList.remove('hide');
     }
 });
+
+const slideCrew = document.querySelectorAll('#slide-crew li button');
+
+async function fetchInfo(index) {
+    let array = Array.from(slideCrew),
+        item = array.indexOf(index);
+
+    fetch('../data.json').then(r => r.json()).then(r => {
+        const image = document.querySelector('#person img'),
+            role = document.getElementById('role'),
+            name = document.getElementById('name'),
+            bio = document.getElementById('bio');
+
+        image.src = r.crew[item].images.png;
+        role.innerText = r.crew[item].role;
+        name.innerText = r.crew[item].name;
+        bio.innerText = r.crew[item].bio;
+    });
+};
+
+slideCrew.forEach(e => {
+    e.addEventListener('click', evt => {
+        slideCrew.forEach(e => e.classList.remove('active'));
+        evt.currentTarget.classList.add('active');
+
+        fetchInfo(evt.currentTarget);
+    });
+});
