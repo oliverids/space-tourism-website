3149f2ce9ecd02f9abcca11cf531da174e891e58
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-11-13 16:25:45 -0300

    main section

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ const toggleMenu = document.getElementById('toggleMenu'),
 
 toggleMenu.addEventListener('click', () => {
     [dropdownMenu, toggleMenu].forEach(e => e.classList.toggle('ativo'))
+});
+
+window.addEventListener('click', e => {
+    if (dropdownMenu.classList.contains('ativo') && !dropdownMenu.contains(e.target) && !toggleMenu.contains(e.target)) {
+        [dropdownMenu, toggleMenu].forEach(e => e.classList.remove('ativo'))
+    }
 })
