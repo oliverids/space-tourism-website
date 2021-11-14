1a362ac05df58e8b98e9fc9114346aa50d6a3fb7
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-11-14 15:45:42 -0300

    technology page script

--- js/technology.js
+++ js/technology.js
@@ -1,36 +1,41 @@
-window.addEventListener('click', e => {
-    let hideBtns = document.querySelector('.slide');
-    if (dropdownMenu.classList.contains('ativo')) {
-        hideBtns.classList.add('hide');
-    } else {
-        hideBtns.classList.remove('hide');
-    }
-});
+const slideTech = document.querySelectorAll('#slide-tech li button');
 
-const slideCrew = document.querySelectorAll('#slide-crew li button');
-
-async function fetchInfo(index) {
-    let array = Array.from(slideCrew),
+async function fetchInfo(index, lista) {
+    let array = Array.from(lista),
         item = array.indexOf(index);
 
     fetch('../data.json').then(r => r.json()).then(r => {
-        const image = document.querySelector('#person img'),
-            role = document.getElementById('role'),
-            name = document.getElementById('name'),
-            bio = document.getElementById('bio');
+        const picture = document.querySelector('#picture img'),
+            nome = document.getElementById('nome'),
+            text = document.getElementById('text');
 
-        image.src = r.crew[item].images.png;
-        role.innerText = r.crew[item].role;
-        name.innerText = r.crew[item].name;
-        bio.innerText = r.crew[item].bio;
+        [nome, text, picture].forEach(e => e.classList.add('anima'));
+
+        setTimeout(() => {
+            nome.innerText = r.technology[item].name;
+            text.innerText = r.technology[item].description;
+            window.innerWidth < 1200 ? picture.src = r.technology[item].images.landscape : picture.src = r.technology[item].images.portrait;
+        }, 210);
+        setTimeout(() => {
+            [nome, text, picture].forEach(e => e.classList.remove('anima'));
+        }, 300);
     });
 };
 
-slideCrew.forEach(e => {
+slideTech.forEach(e => {
     e.addEventListener('click', evt => {
-        slideCrew.forEach(e => e.classList.remove('active'));
+        slideTech.forEach(e => e.classList.remove('active'));
         evt.currentTarget.classList.add('active');
 
-        fetchInfo(evt.currentTarget);
+        fetchInfo(evt.currentTarget, slideTech);
     });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetchInfo(slideTech[0], slideTech)
+});
+let ativo;
+window.addEventListener('resize', () => {
+    ativo = document.querySelectorAll('#slide-tech li button.active');
+    fetchInfo(ativo, slideTech);
 });
